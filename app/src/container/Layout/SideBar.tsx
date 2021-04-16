a060d6e0525473a4d61d8e5cd547f62399b31c57
vidhya1990 <vidhyaa0502@gmail.com>
2021-04-16 02:19:31 -0700

    List User

--- app/src/container/Layout/SideBar.tsx
+++ app/src/container/Layout/SideBar.tsx
@@ -99,6 +99,13 @@ class Sidebar extends Component<Props, States> {
                                             <img src={userListIcon} alt="User" width="16" /> <span> Configurations </span>
                                         </Link>
                                     </li>
+
+                                    <li className="d-flex">
+                                        <span className={activeTab === 'userList' ? 'waves-effect active':'waves-effect'}></span>
+                                        <Link  to="/userList" className={activeTab === 'userList' ? 'waves-effect active':'waves-effect'} onClick={() => this.setActiveTab('userList')}>
+                                            <img src={userListIcon} alt="User" width="16" /> <span> User List </span>
+                                        </Link>
+                                    </li>
                                     
 
                                     <li className="menu-title">LOGS</li>
