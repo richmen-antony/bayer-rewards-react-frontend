2215c47c0a7c4dcc09cdfa5cf945fda2fffb78ba
Dhanasekaran <dhanaseakran.b@cognizant.com>
2021-04-30 11:04:56 +0530

    updated

--- app/src/components/users/userList/index.tsx
+++ app/src/components/users/userList/index.tsx
@@ -611,6 +611,29 @@ handlePaginationChange = (e: any) => {
       );
     }
     const { classes } = this.props;
+    const btn = {
+      background: "#1F445A 0% 0% no-repeat",
+      borderRadius: "100px",
+      opacity: 1,
+      textalign: "left",
+      font: "normal normal normal 16px/22px Open Sans",
+      color: "#FFFFFF",
+      width: 100,
+      height: 35,
+      margin: "15px"
+    }
+
+    const btnD = {
+      background: "#FFFFFF 0% 0% no-repeat",
+      borderRadius: "100px",
+      opacity: 1,
+      textalign: "left",
+      font: "normal normal normal 16px/22px Open Sans",
+      color: "#000000",
+      width: 100,
+      height: 35,
+      margin: "5px"
+    }
 
     return (
       <AUX>
@@ -661,41 +684,45 @@ handlePaginationChange = (e: any) => {
             border: '1px solid #ffffff',
             borderRadius:'.25rem'
             }}>
-              
-          <div className="row align-items-center">
-            <div className="col-sm-6">
-              <div className="searchInputRow advisor-sales">
-                  <i className="fa fa-search icon"></i>
-                  <input
-                    placeholder="Search..[Min 3 chars]"
-                    className="input-field"
-                    type="text"
-                    onChange={this.handleSearch}
-                    value={searchText}
-                  />
-              </div>
-            </div>
-            <div className="col-sm-6 leftAlign">
-              <div className="col-sm-3">
-                <label className="font-weight-bold">Partner Type</label>
-              </div>
-              <div className="col-sm-3">
-              {!changeLogOpen && 
-                (
-                <div className="filterRow">
-                <Dropdown
-                  isOpen={dropdownOpenFilter}
-                  toggle={this.toggleFilter}
-                >
-                  <DropdownToggle>
-                    {!dropdownOpenFilter && (
-                      <img src={filterIcon} width="17" alt="filter" />
-                    )}
-                  </DropdownToggle>
-                  <DropdownMenu right>
-                    <div className="p-3">
-                      <label className="font-weight-bold">
-                        Distributor / Retailer
+
+              <div className="row align-items-center">
+                <div className="col-sm-6">
+                  <div className="searchInputRow advisor-sales">
+                    <i className="fa fa-search icon"></i>
+                    <input
+                      placeholder="Search..[Min 3 chars]"
+                      className="input-field"
+                      type="text"
+                      onChange={this.handleSearch}
+                      value={searchText}
+                    />
+                  </div>
+                </div>
+                <div className="col-sm-6 leftAlign">
+                  <div>
+                    <label className="font-weight-bold">Partner Type</label>
+                    < button type="button" style={btn} >Retailer</button>
+                  </div>
+                  <div>
+                    < button type="button" style={btnD}>Distributor</button>
+                  </div>
+                  <div className="col-sm-3">
+                    {!changeLogOpen &&
+                      (
+                        <div className="filterRow">
+                          <Dropdown
+                            isOpen={dropdownOpenFilter}
+                            toggle={this.toggleFilter}
+                          >
+                            <DropdownToggle>
+                              {!dropdownOpenFilter && (
+                                <img src={filterIcon} width="17" alt="filter" />
+                              )}
+                            </DropdownToggle>
+                            <DropdownMenu right>
+                              <div className="p-3">
+                                <label className="font-weight-bold">
+                                  Distributor / Retailer
                       </label>
                       <i
                         className="fa fa-filter boxed float-right"
