33de978d19bd3fb456f0e5a0d138ed9b7dcbb914
Dhanasekaran <dhanaseakran.b@cognizant.com>
2021-06-10 20:22:11 +0530

    sessiontimeout implementation and isremember implementation

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,20 +1,38 @@
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import {Router} from './router';
-import * as React from 'react';
+import { Router } from "./router";
+import * as React from "react";
+
+// To enable isRemember Need to logged out
+import { getLocalStorageData } from "./utility/base/localStore";
+import Authorization from "./utility/authorization";
 
 // Define Type html tag section declaration
 declare global {
   namespace JSX {
-      interface IntrinsicElements {
-          'h8': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-          'h7': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-      }
+    interface IntrinsicElements {
+      h8: React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+      h7: React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+    }
   }
 }
+
 function App() {
+  // To enable isRemember Need to logged out
+  let data: any = getLocalStorageData("userData");
+  let userinfo = JSON.parse(data);
+  window.onbeforeunload = function (e: any) {
+    if (userinfo?.isRemember === false) Authorization.logOut();
+  };
+
   return (
     <>
       <ToastContainer />
@@ -23,11 +41,7 @@ function App() {
   );
 }
 
-
-
 // interface PersonInfoProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> {
 
- 
 // }
 export default App;
-
