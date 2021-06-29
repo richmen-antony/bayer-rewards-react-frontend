857a6d4e765b319953246e2a6dde428aa94f1736
richmen <richmen624@gmail.com>
2021-06-29 20:18:02 +0530

    jest unit test cased initiated

---
+++ app/src/__tests__/Login.test.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import {Login} from "../components/auth/login";
+describe("<Login />", () => {
+    test("should display a blank login form, with remember me checked by default", async () => {
+      // ???
+    });
+  });
